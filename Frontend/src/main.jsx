655b4d01cf26224ac1b4bf18f776cb122d19ca42
MAyurDongare12 <dongaremayur56@gmail.com>
2026-04-07 21:57:52 +0530

    auto connect backend

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -1,3 +1,11 @@
+const originalFetch = window.fetch;
+
+window.fetch = (url, options) => {
+  if (url.startsWith("/")) {
+    url = "https://gen-ai-job-preparation-web-application.onrender.com" + url;
+  }
+  return originalFetch(url, options);
+};
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
